5b1e2a91b5fa9ba2d3368b2f4bb6bb3ccd2f809a
replicating.rat <replicating.rat@gmail.com>
2014-09-30 18:57:25 +0300

    ввв

--- week2/R/pollutantmean.R
+++ week2/R/pollutantmean.R
@@ -1,5 +1,9 @@
 pollutantmean <- function(directory, pollutant, ids = 1:332) {
+  if(!(pollutant == "sulfate" || pollutant == "nitrate")) {
+    return("fail")
+  }
   trueDir = paste(c(getwd(), "/", directory), collapse='')
+  allMeans <- vector()
   for(id in ids) {
     if(id < 10){
       name = paste(c("00", id), collapse='')
@@ -12,6 +16,8 @@ pollutantmean <- function(directory, pollutant, ids = 1:332) {
     table = read.csv(filename)
     good <- complete.cases(table)
     table <- table[good,]
-    print(table)
+    allMeans <- c(allMeans, mean(table[[pollutant]]))
   }
+  
+  return(mean(allMeans))
 }
