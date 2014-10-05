5e6ac331106624b52add69ab1f94bc60ff32317d
Xenonchik <lordxenon@meta.ua>
2014-10-05 21:24:47 +0300

    Task 3

--- week4/R/rankhospital.R
+++ week4/R/rankhospital.R
@@ -1,13 +1,16 @@
-best <- function(state, outcome) {
+rankhospital <- function(state, outcome, num = "best") {
+  
   vars <- list(heart_attack = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", 
                heart_failure = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure" , 
                pneumonia = "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
-               )
-  outName = vars[[gsub(" ", "_", outcome)]])
+  )
+  outName = vars[[gsub(" ", "_", outcome)]]
   data <- read.csv("week4/outcome-of-care-measures.csv", colClasses = "character")
   data <- data[data$State==state, c("Hospital.Name", outName)]
   data[,outName] <- as.numeric(data[,outName])
   data <- data[complete.cases(data),]
   data <- data[ order(data[,2], data[,1]), ]
-  return(data[1,][[1]])
+  if(num == "best") num <- 1
+  else if(num == "worst") num <- nrow(data)
+  return(data[num,][[1]])
 }
